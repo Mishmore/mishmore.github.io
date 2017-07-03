ed0d73ddd0b5241dee2a371a6e7124452c8f0c7f
mishmore <michellemore28@gmail.com>
2017-07-03 00:35:09 -0500

    project fixed and columns

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,43 @@
 $(_ => {
   $(".button-collapse").sideNav();
 
-  const fixedCol = $('.portfolio .col.s4');
   const fixedNav = $('.navbar-fixed');
+  const portfolioTitle = $('.portfolio h3');
+  const proj1 = $('.project1');
+  const proj2 = $('.project2');
+  const proj3 = $('.project3');
+
   let lastScroll = 0;
-  console.log(fixedCol);
+  console.log(proj1);
 
   $(window).scroll(function(e) {
     const currScroll = $(window).scrollTop();
     console.log(currScroll);
     if (currScroll > lastScroll && currScroll > 150) {
-      console.log('ocultar');
       fixedNav.css('top', '-100px');
-    } else {
-      console.log('mostrar');
+    }
+    else {
       fixedNav.css('top', '0');
     }
      lastScroll = currScroll;
+  });
+
+  $(window).scroll(function(e) {
+    const currScroll = $(window).scrollTop();
+    console.log(currScroll);
+    if (currScroll > 1385) {
+      portfolioTitle.addClass('fixed');
+      portfolioTitle.css('top', '80px');
+      portfolioTitle.css('margin', '0');
+      proj1.css('left', '13.5%');
+    }
+    if (currScroll > 1819) {
+      proj1.css('left', '-350px');
+    }
+    if (currScroll > 2750 || currScroll < 1385) {
+      portfolioTitle.removeClass('fixed');
+      portfolioTitle.css('top', '0');
+      proj1.css('left', '-350px');
+    }
   });
 })
