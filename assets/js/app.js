1ac2673d5dff247a17ddc1ff5c558d72fda4dcfb
mishmore <michellemore28@gmail.com>
2017-07-03 23:01:04 -0500

    saving animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,22 +22,38 @@ $(_ => {
      lastScroll = currScroll;
   });
 
+  const spaceAnimate = 340;
+  const initialAnimate = 1360;
+
   $(window).scroll(function(e) {
     const currScroll = $(window).scrollTop();
     console.log(currScroll);
-    if (currScroll > 1385) {
+    if (currScroll > initialAnimate) {
       portfolioTitle.addClass('fixed');
       portfolioTitle.css('top', '80px');
       portfolioTitle.css('margin', '0');
       proj1.css('left', '13.5%');
+      proj2.css('left', '-350px');
     }
-    if (currScroll > 1819) {
+    if (currScroll > 1700) {
       proj1.css('left', '-350px');
     }
-    if (currScroll > 2750 || currScroll < 1385) {
+    if (currScroll > 2750 || currScroll < 1360) {
       portfolioTitle.removeClass('fixed');
       portfolioTitle.css('top', '0');
       proj1.css('left', '-350px');
+    }
+    if (currScroll > 1836) {
+      proj2.css('left', '13.5%');
+    }
+    if (currScroll > 2346) {
+      proj2.css('left', '-350px');
+    }
+    if (currScroll > 2436) {
+      proj3.css('left', '13.5%');
+    }
+    if (currScroll > 2946) {
+      proj3.css('left', '-350px');
     }
   });
 })
