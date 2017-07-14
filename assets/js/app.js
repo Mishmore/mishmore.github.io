e791e32a3b8aad7e4b6b49eca83009389f82c528
Mishmore <michellemore28@gmail.com>
2017-07-14 12:25:45 -0500

    scrolling with keys ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,6 @@ $(_ => {
 
   $(window).scroll((e) => {
     const currScroll = $(window).scrollTop();
-    //console.log(currScroll);
     if (currScroll > lastScroll && currScroll > 150) {
       fixedNav.css('top', '-100px');
     }
@@ -61,22 +60,29 @@ $(_ => {
       portfolioTitle.css('left', '13.5%');
     }
   });
-})
+});
+
+const downKey = 40;
+const upKey = 38;
+let downCounter = 0;
 
 const YPos = {
   hero       : 0,
   aboutPos   : $("#about").offset().top,
   firstProj  : $("#portfolio").offset().top + 50,
   secondProj : $("#p2").offset().top - 200,
-  thirdProj  : $("#p3").offset().top,
+  thirdProj  : $("#p3").offset().top - 200,
   contactPos : $("#contact").offset().top,
 }
-console.log(YPos.secondProj);
 
-const arrPos = [YPos.hero, YPos.aboutPos, YPos.firstProj, YPos.secondProj, YPos.thirdProj, YPos.contactPos];
+const Scroll = (section) => {
+   $('body').animate({
+       scrollTop: section}
+       ,700);
+}
 
-$(window).on('keyup keypress', (event) => {
-  const currPos = $(window).scrollTop();
+$(window).on('keyup keypress wheel', (event) => {
+  const currPos = $(window).scrollTop() || window.pageYOffset || document.documentElement.scrollTop;
   if (event.which == downKey) {
     if (currPos > YPos.hero && currPos < YPos.aboutPos) {
       Scroll(YPos.aboutPos);
@@ -85,9 +91,16 @@ $(window).on('keyup keypress', (event) => {
       Scroll(YPos.firstProj);
       return false;
     }
-    if (currPos > YPos.firstProj) {
-      console.log(YPos.secondProj + 'segundo proj');
+    if (currPos >= YPos.firstProj && currPos < YPos.secondProj) {
       Scroll(YPos.secondProj);
+      return false;
+    }
+    if (currPos >= YPos.secondProj && currPos < YPos.thirdProj) {
+      Scroll(YPos.thirdProj);
+      return false;
+    }
+    if (currPos >= YPos.thirdProj && currPos < YPos.contactPos) {
+      Scroll(YPos.contactPos);
       return false;
     }
   }
@@ -104,76 +117,11 @@ $(window).on('keyup keypress', (event) => {
     if (currPos <= YPos.secondProj && currPos > YPos.firstProj) {
       Scroll(YPos.firstProj);
     }
-  }
-});
-
-const aboutPos = $("#about").offset().top;
-const firstProj = 1512;
-const secondProj = 2308;
-const thirdProj = 2980;
-const contactPos = 3777;
-const downKey = 40;
-const upKey = 38;
-const currScroll = $(window).scrollTop();
-let downCounter = 0;
-$("#about").offset().top;
-
-/*
-$(window).on('keydown', (event) => {
-
-  if (event.which == upKey) {
-    --downCounter;
-    doScrollKey();
-    //console.log('up'+downCounter);
-  }
-  else if (event.which == downKey ) {
-    ++downCounter;
-    doScrollKey();
-
-    //console.log('down'+downCounter);
-  }
-});
-*/
-
-/*
-$(window).scroll(function(e) {
-  let lastScroll = 0;
-  const currScroll = $(window).scrollTop();
-    if (currScroll > 0 && lastScroll < aboutPos) {
-      downCounter = 0;
+    if (currPos <= YPos.thirdProj && currPos > YPos.secondProj) {
+      Scroll(YPos.secondProj);
     }
-    if (currScroll >= aboutPos && lastScroll < firstProj) {
-      downCounter = 1;
+    if (currPos <= YPos.contactPos && currPos > YPos.thirdProj) {
+      Scroll(YPos.thirdProj);
     }
-});
-*/
-const doScrollKey = () => {
-  switch (downCounter) {
-    case 0:
-    Scroll(aboutPos);
-    //downCounter++;
-    break;
-    case 1:
-    Scroll(firstProj);
-    //downCounter++;
-    break;
-    case 2:
-    Scroll(secondProj);
-    //downCounter++;
-    break;
-    case 3:
-    Scroll(thirdProj);
-    //downCounter++;
-    break;
-    case 4:
-    Scroll(contactPos);
-    //downCounter++;
-    break;
   }
-}
-
-const Scroll = (section) => {
-   $('body').animate({
-       scrollTop: section}
-       ,700);
-}
+});
