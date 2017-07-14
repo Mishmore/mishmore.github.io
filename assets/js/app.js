d43e108aeb79968f4bd67002b909025e0947440b
mishmore <michellemore28@gmail.com>
2017-07-14 00:28:32 -0500

    scroll work

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,11 +8,10 @@ $(_ => {
   const proj3 = $('.project3');
 
   let lastScroll = 0;
-  console.log(proj1);
 
-  $(window).scroll(function(e) {
+  $(window).scroll((e) => {
     const currScroll = $(window).scrollTop();
-    console.log(currScroll);
+    //console.log(currScroll);
     if (currScroll > lastScroll && currScroll > 150) {
       fixedNav.css('top', '-100px');
     }
@@ -27,7 +26,7 @@ $(_ => {
 
   $(window).scroll(function(e) {
     const currScroll = $(window).scrollTop();
-    console.log(currScroll);
+    //console.log(currScroll);
     if (currScroll < initialAnimate) {
       portfolioTitle.removeClass('fixed');
     }
@@ -64,13 +63,70 @@ $(_ => {
   });
 })
 
-let aboutPos = 671;
+const aboutPos = 671;
+const firstProj = 1512;
+const secondProj = 2308;
+const thirdProj = 2980;
+const contactPos = 3777;
 const downKey = 40;
+const upKey = 38;
+const currScroll = $(window).scrollTop();
+let downCounter = 0;
+
 
 $(window).on('keydown', (event) => {
-  if (event.which == downKey) {
-     $('body').animate({
-         scrollTop: aboutPos}
-         ,700);
+  if (event.which == upKey) {
+    //downCounter--;
+    console.log('up'+downCounter);
+    doScrollKey();
+    console.log('up'+downCounter);
+  }
+  else if (event.which == downKey ) {
+    console.log('down'+downCounter);
+    doScrollKey();
+    //downCounter++;
+    console.log('down'+downCounter);
+  }
+});
+
+$(window).scroll(function(e) {
+  let lastScroll = 0;
+  const currScroll = $(window).scrollTop();
+    if (currScroll > 0 && lastScroll < aboutPos) {
+      downCounter = 0;
+    }
+    if (currScroll >= aboutPos && lastScroll < firstProj) {
+      downCounter = 1;
     }
 });
+
+const doScrollKey = () => {
+  switch (downCounter) {
+    case 0:
+    Scroll(aboutPos);
+    //downCounter++;
+    break;
+    case 1:
+    Scroll(firstProj);
+    //downCounter++;
+    break;
+    case 2:
+    Scroll(secondProj);
+    //downCounter++;
+    break;
+    case 3:
+    Scroll(thirdProj);
+    //downCounter++;
+    break;
+    case 4:
+    Scroll(contactPos);
+    //downCounter++;
+    break;
+  }
+}
+
+const Scroll = (section) => {
+   $('body').animate({
+       scrollTop: section}
+       ,700);
+}
